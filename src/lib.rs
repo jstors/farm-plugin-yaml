53884c9366a081fbfae54fa9f38a98ab06a16a6c
Liuqh233 <528851597@qq.com>
2024-06-26 15:08:30 +0800

    chore: reactor (#4)

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 #![deny(clippy::all)]
-
 use std::collections::HashMap;
 
 use farmfe_core::{
@@ -10,13 +9,22 @@ use farmfe_core::{
 };
 use farmfe_macro_plugin::farm_plugin;
 use farmfe_toolkit::fs;
+use serde::{Deserialize, Serialize};
 use serde_yaml::Value;
 
 #[farm_plugin]
 pub struct FarmPluginYaml {}
 
+#[derive(Debug, Deserialize, Serialize)]
+enum ResultValue {
+  List(Vec<Value>),
+  Obj(HashMap<Value, Value>),
+  Str(String),
+  Value(serde_yaml::Value),
+}
+
 impl FarmPluginYaml {
-  fn new(config: &Config, options: String) -> Self {
+  pub fn new(_config: &Config, _options: String) -> Self {
     Self {}
   }
 }
@@ -46,23 +54,46 @@ impl Plugin for FarmPluginYaml {
     _context: &std::sync::Arc<farmfe_core::context::CompilationContext>,
   ) -> farmfe_core::error::Result<Option<farmfe_core::plugin::PluginTransformHookResult>> {
     if param.module_type == ModuleType::Custom(("yaml").to_string()) {
-      let value: Value = Value::String(param.content.to_owned());
-      let yaml_str: String = serde_yaml::from_value(value).expect("Failed to parse YAML");
-      // Split the string into key-value pairs.
-      let key_values: Vec<&str> = yaml_str.split("\n").collect();
-      let mut data = HashMap::new();
+      let value = serde_yaml::from_str(&param.content).expect("Failed parse YAML");
+      let mut result: HashMap<String, serde_json::Value> = HashMap::new();
 
-      for pair in key_values {
-        let parts: Vec<&str> = pair.split(":").collect();
-        if parts.len() == 2 {
-          let key: &str = parts[0].trim();
-          let value = parts[1].trim();
-          data.insert(key.to_string(), value.to_string());
+      if let Value::Mapping(map) = value {
+        for (k, v) in map {
+          match v {
+            Value::Sequence(seq) => {
+              result.insert(
+                key_to_string(&k),
+                serde_json::Value::Array(
+                  serde_json::to_value(seq)
+                    .unwrap()
+                    .as_array()
+                    .unwrap()
+                    .to_owned(),
+                ),
+              );
+            }
+            Value::Mapping(inner_map) => {
+              let mut inner_obj = serde_json::Map::new();
+
+              for (k, v) in inner_map {
+                inner_obj.insert(
+                  key_to_string(&k),
+                  serde_json::to_value(trim_str(serde_yaml::to_string(&v).unwrap())).unwrap(),
+                );
+              }
+              result.insert(key_to_string(&k), serde_json::Value::Object(inner_obj));
+            }
+            _ => {
+              result.insert(
+                key_to_string(&k),
+                serde_json::Value::String(trim_str(serde_yaml::to_string(&v).unwrap())),
+              );
+            }
+          }
         }
-      }
+      };
 
-      let json_data = json!(data);
-      let result_code = format!("module.exports = {}", json_data);
+      let result_code = format!("module.exports = {}", json!(&result));
 
       return Ok(Some(PluginTransformHookResult {
         content: result_code,
@@ -74,4 +105,12 @@ impl Plugin for FarmPluginYaml {
 
     Ok(None)
   }
+}
+
+fn key_to_string(key: &Value) -> String {
+  serde_yaml::to_string(key).unwrap().trim().to_string()
+}
+
+fn trim_str(str: String) -> String {
+  str.trim().to_string()
 }
